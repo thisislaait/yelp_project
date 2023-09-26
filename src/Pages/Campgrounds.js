114a29d3c196fd795a0bc716953c24b02569da09
thisislaait <ogadi.ugo@gmail.com>
2023-09-27 00:31:03 +0200

    Data set

--- src/Pages/Campgrounds.js
+++ src/Pages/Campgrounds.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Nav from "../Components/Nav";
 import Banner from "../Components/Banner";
+import campgroundData from "../Data/campgroundData";
+import campgroundCard from "../Data/campgroundCard";
 
 const Campgrounds = () => {
   return (
@@ -10,7 +12,9 @@ const Campgrounds = () => {
         <Banner />
       </section>
       <section className="row card-content mx-12 md:mx-20 flex items-center flex-wrap md:flex gap-2.5 mb-10">
-        {/* Cards for mobile and tablet */}
+        {campgroundData.map((campground) => (
+            <campgroundCard key={campground.id} campground={campground} />
+        ))}
       </section>
     </main>
   );
