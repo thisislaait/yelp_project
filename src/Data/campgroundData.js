3683fb3fff9f32acbe31b06c395d8c8d983e85e7
thisislaait <ogadi.ugo@gmail.com>
2023-09-27 03:28:48 +0200

    campground updated

--- src/Data/campgroundData.js
+++ src/Data/campgroundData.js
@@ -2,10 +2,50 @@ const campgroundData = [
     {
       id: 1,
       title: "Mount Ulap",
-      description: "One of the most famous hikes in Benguet is Mt Ulap.",
-      imageUrl: "../Assets/Camp Images/Mount Ulap.jpg",
+      des: "One of the most famous hikes in Benguet is Mount Ulap.",
+      img: "/Assets/Camp Images/Mount Ulap.jpg",
       route: "mount-ulap",
     },
+
+    {
+      id: 2,
+      title: "Calaguas Island",
+      des: "A paradise that rivals the white sand beauty of Boracay.",
+      img: "/Assets/Camp Images/Calaguas Island.jpg", 
+      route: "calaguas-island.html",
+    },
+     
+    {
+      id: 3,
+      title: "Onay Beach",
+      des: "One of the best beach camp sites, beautiful and pristine.",
+      img: "/Assets/Camp Images/Onay Beach.jpg", 
+      route: "onay-beach.html",
+    },
+
+    {
+      id: 4,
+      title: "Seven Sisters Waterfall",
+      des: "The Seven Sisters is the 39th tallest waterfall in Norway",
+      img: "/Assets/Camp Images/Seven Sisters Waterfall.jpg", 
+      route: "seven-sisters-waterfall.html",
+    },
+   
+    {
+      id: 5,
+      title: "Latik Riverside",
+      des: "Philippines is one of the most dazzling countries in Asia",
+      img: "/Assets/Camp Images/Latik Riverside.jpg", 
+      route: "latik-riverside.html",
+    },
+   
+    {
+      id: 6,
+      title: "Buloy Springs",
+      des: "One of the best beach camp sites, beautiful and pristine.",
+      img: "/Assets/Camp Images/Buloy Springs.jpg", 
+      route: "buloy-springs.html",
+    }
    
   ];
   
--- src/Pages/Campgrounds.js
+++ src/Pages/Campgrounds.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Nav from "../Components/Nav";
 import Banner from "../Components/Banner";
 import campgroundData from "../Data/campgroundData";
-import CampgroundCard from "../Data/CampgroundCard";
+import CampgroundCard from "../Data/Cards";
 
 const Campgrounds = () => {
   return (
@@ -13,7 +13,7 @@ const Campgrounds = () => {
       </section>
       <section className="row card-content mx-12 md:mx-20 flex items-center flex-wrap md:flex gap-2.5 mb-10">
         {campgroundData.map((campground) => (
-            <CampgroundCard key={campground.id} campground={campground} /> // Corrected component name
+            <CampgroundCard key={campground.id} campground={campground} />
         ))}
       </section>
     </main>
